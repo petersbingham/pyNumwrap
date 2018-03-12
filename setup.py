ec0c72c2a4b89812bf17ce412943abb6f324e9ab
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-12 22:36:44 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='pynumwrap',
-      version='0.4',
-      description='Simple Numerical Utilities',
+      version='0.5',
+      description='Python package wrapping python and mpmath types behind a common interface',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['pynumwrap'],
