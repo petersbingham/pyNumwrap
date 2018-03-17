f5fb3cdb779aa48f75ec3817ebc9b7881e5c149c
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:06:26 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'pynumwrap/README.md')
 
 setup(name='pynumwrap',
-      version='0.7',
+      version='0.8',
       description='Python package wrapping python and mpmath types behind a common interface',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['pynumwrap'],
+      package_data={'pynumwrap': ['README.md'],
      )
